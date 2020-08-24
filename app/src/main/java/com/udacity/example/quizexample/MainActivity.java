1429ad3f5a748de3f17c2a6b535b634291cf23d2
Jonathan Lin <69509330+Jonathan-Lin-android@users.noreply.github.com>
2020-08-24 16:27:36 -0700

    implemented a background thread to fetch data using content provider. word and definition shows correctly

--- app/src/main/java/com/udacity/example/quizexample/MainActivity.java
+++ app/src/main/java/com/udacity/example/quizexample/MainActivity.java
@@ -16,22 +16,29 @@
 
 package com.udacity.example.quizexample;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import com.udacity.example.droidtermsprovider.DroidTermsExampleContract;
 
 /**
  * Gets the data from the ContentProvider and shows a series of flash cards.
  */
 
 public class MainActivity extends AppCompatActivity {
-
     // The current state of the app
     private int mCurrentState;
 
-
     private Button mButton;
+
+    private TextView mTextViewWord;
+
+    private TextView mTextViewDefinition;
 
     // This state is when the word definition is hidden and clicking the button will therefore
     // show the definition
@@ -41,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     // advance the app to the next word
     private final int STATE_SHOWN = 1;
 
+    private Cursor mData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +57,15 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the views
         mButton = (Button) findViewById(R.id.button_next);
+        mTextViewWord = (TextView) findViewById(R.id.text_view_word);
+        mTextViewDefinition = (TextView) findViewById(R.id.text_view_definition);
+        new FetchTask().execute();
     }
 
     /**
      * This is called from the layout when the button is clicked and switches between the
      * two app states.
+     *
      * @param view The view that was clicked
      */
     public void onButtonClick(View view) {
@@ -71,21 +83,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void nextWord() {
-
         // Change button text
         mButton.setText(getString(R.string.show_definition));
-
+        mTextViewDefinition.setVisibility(View.INVISIBLE);
         mCurrentState = STATE_HIDDEN;
 
+        setWordDefinition();
     }
 
     public void showDefinition() {
-
         // Change button text
         mButton.setText(getString(R.string.next_word));
-
+        mTextViewDefinition.setVisibility(View.VISIBLE);
         mCurrentState = STATE_SHOWN;
-
     }
 
+    public class FetchTask extends AsyncTask<Void, Void, Cursor> {
+
+        @Override
+        protected Cursor doInBackground(final Void... voids) {
+            ContentResolver resolver = getContentResolver();
+            //content profvider prefix + content authority + specific data, projection, selection, selection args, sort order
+            Cursor cursor = resolver.query(DroidTermsExampleContract.CONTENT_URI, null, null, null, null);
+            return cursor;
+        }
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            mData = cursor;
+            setWordDefinition();
+            // initialize the word and definition.
+        }
+    }
+
+    private void setWordDefinition()
+    {
+        if(mData == null)
+            return;
+
+        int wordCol = mData.getColumnIndex(DroidTermsExampleContract.COLUMN_WORD);
+        int definitionCol = mData.getColumnIndex(DroidTermsExampleContract.COLUMN_DEFINITION);
+        if(!mData.moveToNext())
+            mData.moveToFirst();
+
+            String word = mData.getString(wordCol);
+            String definition = mData.getString(definitionCol);
+
+            mTextViewWord.setText(word);
+            mTextViewDefinition.setText(definition);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mData.close();
+    }
 }
